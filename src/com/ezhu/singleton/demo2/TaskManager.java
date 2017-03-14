38125406cc051f4733d62d52a7b7193ddf1c1ebb
huguge <huguge78@gmail.com>
2017-03-14 17:08:27 +0800

    单例的一个简单的介绍，关于任务管理器。

--- src/com/ezhu/singleton/demo2/TaskManager.java
+++ src/com/ezhu/singleton/demo2/TaskManager.java
@@ -2,8 +2,11 @@ package com.ezhu.singleton.demo2;
 
 public class TaskManager {
 
+	// 创建并保存唯一实例
+	private static TaskManager tm = null;
+	
 	/*
-	 * 构造函数
+	 * 初始化窗口
 	 * 为了保证实例的唯一性，进制外部进行实例化，所以，将构造函数的可见性改为private
 	 */
 	private TaskManager() {
@@ -24,5 +27,14 @@ public class TaskManager {
 		
 	}
 	
+	/*
+	 * 使用公有的静态方法使外界可以访问这个唯一实例
+	 */
+	public static TaskManager getInstance() {
+		if (tm == null) {
+			tm = new TaskManager();
+		}
+		return tm;
+	}
 	
 }
