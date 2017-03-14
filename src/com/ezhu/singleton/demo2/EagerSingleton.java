b0ca3fa6b69af49f60239e7220d30b709673c213
huguge <huguge78@gmail.com>
2017-03-14 17:45:26 +0800

    添加饿汉式EagerSingleton类。

---
+++ src/com/ezhu/singleton/demo2/EagerSingleton.java
@@ -0,0 +1,15 @@
+package com.ezhu.singleton.demo2;
+
+public class EagerSingleton {
+
+	private static EagerSingleton instance = new EagerSingleton();
+	
+	private EagerSingleton() {
+		
+	}
+	
+	public static EagerSingleton getInstance() {
+		return instance;
+	}
+	
+}
